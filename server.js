2ca05f3996ab32d6170d1b92126cbf79ca4f361f
kimdagus06 <guki23tl@student.ju.se>
2024-09-20 10:00:29 +0200

    save

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
-const express = require(express);
+const express = require("express");
+const sqlite3 = require("sqlite3");
 const path = require('path');
 const app = express(); 
-const port = 3000; 
+app.use(express.static("public")); 
+const { engine } = require("express-handlebars");
+const dbFile = "data.sqlite3.db"; 
+db = new sqlite3.Database(dbFile); 
+const port = 3030; 
+
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
 
 const userName = '';
+
 
 app.use(express.static(path.join(__dirname, 'public'))); 
 
 /* https://stackoverflow.com/questions/62092010/use-nodejs-to-change-what-text-html-displays */
 
-app.get('/', (req, res) =>{
-    res.render('home');
-});
+app.get("/", function (req, res) {
+    res.render("home.handlebars"); 
+}); 
 
-app.get('/', (req, res) =>{
-    res.render('events');
+app.get("/contact", function (req, res) {
+    res.render("contact.handlebars"); 
 });
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
