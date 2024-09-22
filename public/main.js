ce196e5eee83398817d4aa5901e3fed412b83ec3
kimdagus06 <guki23tl@student.ju.se>
2024-09-22 11:39:01 +0200

    design fixing

--- public/main.js
+++ public/main.js
@@ -1,28 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const menuItems = document.querySelectorAll('.menu-item');
-
-    menuItems.forEach(item => {
-        item.addEventListener('click', function(event) {
-            event.preventDefault(); // Prevent default link behavior
-
-            // Find a submenu of the clicked menu
-            const submenu = this.nextElementSibling;
-
-            // Hide all submenus 
-            document.querySelectorAll('.submenu').forEach(sub => {
-                if (sub !== submenu) {
-                    sub.classList.remove('show');
-                }
-            });
-
-            // Toggle a clicked menu's submenus 
-            if (submenu && submenu.classList.contains('submenu')) {
-                submenu.classList.toggle('show');
-            }
-        });
-    });
-});
-
 /**
  * Events page
  * When the Undo button is clicked, the event form will be reset.
