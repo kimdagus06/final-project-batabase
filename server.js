2d23fb080a144e5ffbb52442cfd148569d6b8127
kimdagus06 <guki23tl@student.ju.se>
2024-09-23 19:59:05 +0200

    save log in/create account part

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ app.set("views", "./views");
 app.use(express.urlencoded({ extended: true }));
 
 // Create a table
+/**
+ * NOT NULL: Can't have a NULL value. So it can't be left empty.
+ * UNIQUE: All values in a column are distinct from each other. So no two users can have the same email address.
+ */
 db.serialize(() => {
     db.run(`
         CREATE TABLE IF NOT EXISTS users (
@@ -33,6 +37,17 @@ db.serialize(() => {
     });
 });
 
+/**
+ * Session middleware
+ * Update the session middleware to store the user in the session 
+ */
+app.use((req, res, next) => {
+    if (req.session.user) {
+        res.locals.user = req.session.user;
+    }
+    next (); 
+}); 
+
 // https://coda.io/@peter-sigurdson/lab-workbook-setting-up-a-node-js-express-server-with-sqlite-and
 https://www.luisllamas.es/en/how-to-use-sqlite-with-nodejs/
 
@@ -43,6 +58,10 @@ app.get("/", function (req, res) {
 
 app.get("/createaccount", function (req, res) {
     res.render("createaccount"); 
+});
+
+app.get("/login", function (req, res) {
+    res.render("login"); 
 });
 
 app.get("/about", function (req, res) {
@@ -61,33 +80,54 @@ app.get("/events", function (req, res) {
  * Hash Passwords with bcrypt in Node.js
  * https://www.freecodecamp.org/news/how-to-hash-passwords-with-bcrypt-in-nodejs/
  */
-// Handle account creation
 
-
-// 모든 정보를 hashing 해둘 필요 없음 이 부분 fix 해야함 왜냐하면 해킹 같은거 당햇을때 정보복구가 힘들고 
-// 그냥 password만 해싱하면 됨
+// // It's not necessary to hash all information because recovering data can be difficult in the event of a hack. 
+// Just hashing the password is sufficient.
 
 // node ./bcrypt-demo.js
 // getSalt method
 
-app.post("/create-account", (req, res) => {
+app.post("/create-account", async (req, res) => {
     const { first_name, last_name, email, date_of_birth, password } = req.body;
 
-    // Hash the password before saving to the database
-    bcrypt.hash(password, 10, (err, hash) => {
-        if (err) {
-            return res.status(500).send("Error hashing password: " + err.message);
-        }
+    try {
+        // Hash the password before saving to the database
+        const hash = await bcrypt.hash(password, 14); 
 
         const sql = `INSERT INTO users (first_name, last_name, email, date_of_birth, password) VALUES (?, ?, ?, ?, ?)`;
         
         db.run(sql, [first_name, last_name, email, date_of_birth, hash], (err) => {
             if (err) {
-                return res.status(500).send("Error creating account: " + err.message);
+                return res.status(500).send("Server error: " + err.message);
             }
-            res.redirect("/");
+            res.redirect("/main");
         });
-    });
+    } catch (err) {
+        return res.status(500).send("Error hashing password: " + err.message);
+    }
+});
+
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    // Find the user in the database
+    db.get('SELECT * FROM users WHERE email = ?', [email], async (err, user) => {
+        if (err) {
+            res.status(500).send('Server error');
+        } else if (!user) {
+            res.status(401).send('User not found');
+        } else {
+            
+    const result = await bcrypt.compare(password, user.password);
+    
+    if (result) {
+        req.session.user = user; // Store the user in the session
+        res.redirect('/'); // Redirect to the home page
+        } else {
+            res.status(401).send('Wrong password');
+        }
+    }
+});
 });
 
 /* Start a port */
